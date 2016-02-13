160bb47d5cd688628f98abeb9d70549652fb51bc
taha <taha.ziadeh@navybits.com>
2016-02-13 11:37:14 +0200

    first commit version 0.0.1

---
+++ README.md
@@ -0,0 +1,35 @@
+# navybits:bootstrap-icon-picker
+___
+*navybits:bootstrap-icon-picker* is a simple wrapper on [bootstrap-iconpicker](http://victor-valencia.github.io/bootstrap-iconpicker/) that supports fontawsome icons picker packaged for Meteor Usage.
+### Dependencies
+  - jquery
+  - twbs:bootstrap
+### Installation:
+```sh
+meteor add navybits:bootstrap-icon-picker
+```
+### Use
+1. Inside your template, for example *myIconPickerTemplate.html* :
+```
+<template name="myIconPickerTemplate">
+<button class="btn btn-default" data-iconset="fontawesome" data-icon="fa-globe" role="iconpicker"></button>
+</template>
+```
+2. In your template controller, for example *myIconPickerTemplate.js*:
+```javascript
+Template.myIconPickerTemplate.rendered = function() {
+  $('button[role="iconpicker"]').iconpicker();
+  }
+Template.myIconPickerTemplate.events({
+  'change button[role="iconpicker"]':function (e) {
+    console.log(e.icon);
+  }
+  });
+```
+
+### Version
+0.0.1
+
+### License
+
+MIT
---
+++ package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  name: 'navybits:bootstrap-iconpicker',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Very simple wrapper on bootstrap-iconpicker witch supports fontawsome icons picker',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/Navybits/bootstrap-iconpicker',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use(['jquery', 'twbs:bootstrap'], 'client');
+  api.addFiles('iconset-fontawesome-4.2.0.min.js', 'client');
+  api.addFiles('bootstrap-iconpicker.min.js', 'client');
+  api.addFiles('bootstrap-iconpicker.min.css', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use(['jquery', 'twbs:bootstrap'], 'client');
+  api.addFiles('iconset-fontawesome-4.2.0.min.js', 'client');
+  api.addFiles('bootstrap-iconpicker.min.js', 'client');
+  api.addFiles('bootstrap-iconpicker.min.css', 'client');
+});
