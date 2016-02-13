dc5dd8bcf0b15d384038456bf337391e85a0e56b
taha <taha.ziadeh@navybits.com>
2016-02-13 11:59:16 +0200

    updated twbs:bootstrap package version dependency

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.use(['jquery', 'twbs:bootstrap'], 'client');
+  api.use(['jquery', 'twbs:bootstrap@3.3.6'], 'client');
   api.addFiles('iconset-fontawesome-4.2.0.min.js', 'client');
   api.addFiles('bootstrap-iconpicker.min.js', 'client');
   api.addFiles('bootstrap-iconpicker.min.css', 'client');
