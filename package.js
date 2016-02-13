7d3f6544d7b64af1d94f1da2cf68bca7b1d968e0
taha <taha.ziadeh@navybits.com>
2016-02-13 11:49:58 +0200

    package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'navybits:bootstrap-iconpicker',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'Very simple wrapper on bootstrap-iconpicker witch supports fontawsome icons picker',
   // URL to the Git repository containing the source code for this package.
